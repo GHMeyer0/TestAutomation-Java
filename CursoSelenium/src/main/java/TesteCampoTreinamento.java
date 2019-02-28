470f81e6d9b08a42b89347351f5f3ef195806050
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-02-28 00:25:53 -0300

    Adicionado localização dinamica do html

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TesteCampoTreinamento {
 
 	@Test
-	public void Teste()
+	public void Teste() throws InterruptedException
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://google.com.br");
+		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		
 		
-		driver.quit();
+		//driver.quit();
 	}
 }
