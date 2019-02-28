be26ce947673d36757bfbd0c295ebbb913ad7880
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-02-28 00:42:32 -0300

    Adicionado teste em TextField

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -1,17 +1,32 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
 
 	@Test
-	public void Teste() throws InterruptedException
+	public void testeTextField() throws InterruptedException
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Gabriel");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Helko Meyer");
+		
+		Assert.assertEquals("Gabriel", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		
 		
-		//driver.quit();
+		
+		driver.quit();
+	}
+	
+	public void deveInteragirComTextArea() 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().fullscreen();
+		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
 	}
 }
