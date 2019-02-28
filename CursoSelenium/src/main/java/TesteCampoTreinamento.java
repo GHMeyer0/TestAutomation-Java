fda9e71ca7136fbe0e504c05e1a32af7e9164df4
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-02-28 00:17:13 -0300

    Incluido estrutura basica do teste

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -1,4 +1,17 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
 
+	@Test
+	public void Teste()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com.br");
+		
+		
+		driver.quit();
+	}
 }
