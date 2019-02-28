e621a446afdb0158f6c4a6208f51a806bf0fadac
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-02-27 23:30:35 -0300

    Criado teste para abrir o google

---
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+	
+	@Test
+	public void teste() 
+	{
+		//System.setProperty("webdrive.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://google.com");
+		System.out.println(driver.getTitle());
+	}
+	
+	
+}
