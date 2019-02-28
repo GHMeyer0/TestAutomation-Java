6b664c569e70ba8a4b4fb5d18058fe756ff548cd
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-02-28 12:59:00 -0300

    Ajustavel variavel global, e adicionado teste de compo tipo Radio

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -6,19 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
 
+	String baseUrl = "file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html";
+	
 	@Test
 	public void testeTextField() throws InterruptedException
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.get(baseUrl);
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Gabriel");
 		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Helko Meyer");
-		
 		Assert.assertEquals("Gabriel", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
-		
-		
-		
 		driver.quit();
 	}
 	
@@ -26,11 +24,21 @@ public class TesteCampoTreinamento {
 	public void deveInteragirComTextArea() 
 	{
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().fullscreen();
-		driver.get("file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste de Area");
 		Assert.assertEquals("Teste de Area", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		driver.quit();
 		
+	}
+	
+	@Test
+	public void deveInteragirComRadioButton() 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		driver.quit();
 	}
 }
