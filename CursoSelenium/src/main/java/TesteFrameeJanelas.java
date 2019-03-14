092b4a19f00636de0fe1ccfdf86ca774ebe99ef0
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-03-13 23:15:33 -0300

    Criado teste para lidar com iframe e janelas

---
+++ CursoSelenium/src/main/java/TesteFrameeJanelas.java
@@ -0,0 +1,45 @@
+import org.junit.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteFrameeJanelas {
+	String baseUrl = "file://" + System.getProperty("user.dir") + "/src/main/resources/componentes.html";
+	WebDriver driver = new ChromeDriver();
+	
+	private void iniciaBrowser() 
+	{
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
+	}
+	
+	@Test
+	public void deveInteragirComFrames() {
+		this.iniciaBrowser();
+		driver.switchTo().frame("frame1");
+		driver.findElement(By.id("frameButton")).click();
+		Alert alert = driver.switchTo().alert();
+		String msg = alert.getText();
+		Assert.assertEquals("Frame OK!", msg);
+		alert.accept();
+		
+		driver.switchTo().defaultContent();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys(msg);
+		driver.quit();
+	}
+	
+	@Test
+	public void DeveInteragirComJanelas () 
+	{
+		this.iniciaBrowser();
+		driver.findElement(By.id("buttonPopUpEasy")).click();
+		driver.switchTo().window("Popup");
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+		driver.close();
+		driver.switchTo().window("");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Simmm");
+		//driver.quit();
+	}
+
+}
